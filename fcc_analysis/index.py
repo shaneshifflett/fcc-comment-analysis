3b7882ac3a382a0166029f39e1961b4a7e88f4e3
Shane <shifflett.shane@gmail.com>
2017-11-22 20:34:00 +0000

    get rid of qING

--- fcc_analysis/index.py
+++ fcc_analysis/index.py
@@ -7,6 +7,7 @@ import time
 import warnings
 import multiprocessing
 from tqdm import tqdm
+import glob
 
 import requests
 
@@ -28,96 +29,32 @@ class CommentIndexer:
         self.fcc_endpoint = 'https://ecfsapi.fcc.gov/filings'
 
     def run(self):
-        index_queue = multiprocessing.Queue()
-
-        bulk_index_process = multiprocessing.Process(
-            target=self.bulk_index, args=(index_queue,),
-        )
-        bulk_index_process.start()
-        total = self.get_total()
-        if not total:
-            print('error loading document total; using estimate')
-            total = 5000000
+        cnt = 0
+        files = glob.glob("/home/shane/fcc-comment-analysis/data/*.json")
+        print('FILES', len(files))
+        total = len(files)
         progress = tqdm(total=total)
-
-        #for comment in self.iter_comments():
-        with open('/home/shane/fcc-comment-analysis/data/2918000.json') as data_file:    
+        for idx, fname in enumerate(files):
+            with open(fname) as data_file:
                 data = json.load(data_file)
-                for comment in data['filings']:
-                    index_queue.put(comment)
-                    progress.update(1)
-
-        index_queue.put(None)
-        bulk_index_process.join()
+                self.bulk_index_noq(data['filings'])
+                progress.update(1)
         progress.close()
 
-    def build_query(self):
-        query = {
-            'proceedings.name': '17-108',
-            'sort': self.sort
-        }
-        if self.lte and self.gte:
-            query['date_received'] = '[gte]{gte}[lte]{lte}'.format(
-                gte=self.gte,
-                lte=self.lte
-            )
-        return query
-
-    def get_total(self):
-        query = self.build_query()
-        query['limit'] = 1
-        response = requests.get(self.fcc_endpoint, params=query)
-        try:
-            agg = response.json().get('aggregations', {})
-            if not agg:
-                return None
-            for bucket in agg.get('proceedings_name', {}).get('buckets', []):
-                if bucket['key'] == query['proceedings.name']:
-                    return bucket['doc_count']
-        except json.decoder.JSONDecodeError:
-            return None
-        return None
-
-
-    def iter_comments(self, page=0):
-        query = self.build_query()
-        for page in itertools.count(0):
-            query.update({
-                'limit': self.limit,
-                'offset': page * self.limit,
-            })
-            for i in range(7):
-                response = requests.get(self.fcc_endpoint, params=query)
-
-                try:
-                    filings = response.json().get('filings', [])
-                except json.decoder.JSONDecodeError:
-                    # Exponentially wait--sometimes the API goes down.
-                    time.sleep(math.pow(2, i))
-                    continue
-                else:
-                    break
-            for filing in filings:
-                yield filing
-
-            if len(filings) != self.limit:
-                break
-
-    def bulk_index(self, queue):
+    def bulk_index_noq(self, documents):
         endpoint = '{}{}/filing/{}'.format(
             self.endpoint,
             'fcc-comments',
             '_bulk'
         )
-        print('ENDPOINT', endpoint)
         payload = io.StringIO()
         payload_size = 0
         created = False
 
         headers = headers = {'Content-type': 'application/x-ndjson', 'Accept': 'text/plain'}
         
-        while True:
-            document = queue.get()
+        for document in documents:
+            #document = queue.get()
             if document is None:
                 break
 
@@ -150,7 +87,7 @@ class CommentIndexer:
             response = requests.post(endpoint, data=payload.getvalue(), verify=self.verify, headers=headers)
             payload = io.StringIO()
             payload_size = 0
-            print('JSON', response.json())
+            #print('JSON', response.json())
             for item in response.json()['items']:
                 if item['create']['status'] == 201:
                     created = True
